3f18c399757a44e07d34bad75a1c61465f2eb7b4
Tazzmanian <banskaliqtabg@yahoo.com>
2025-08-02 17:13:43 +0300

    fix typos

---
+++ src/main/java/com/employees/pairs/TeodorTodorovEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.employees.pairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeodorTodorovEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeodorTodorovEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employees/pairs/TeodorTodorovEmpployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employees.pairs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeodorTodorovEmpployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
